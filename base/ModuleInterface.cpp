755ca1ff3ea60109f7467bbab1f740913a32f459
Luca Longinotti <l@longi.li>
2017-03-19 22:57:10 +0100

    ModuleInterface: start work on interface to plugin system.

---
+++ base/ModuleInterface.cpp
@@ -0,0 +1,14 @@
+#include "ModuleInterface.hpp"
+
+namespace caer {
+
+ModuleInterface::ModuleInterface() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ModuleInterface::~ModuleInterface() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace caer */
