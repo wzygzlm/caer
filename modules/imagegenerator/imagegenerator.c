a70e9a776332a659f721e45abda5af9eb041500a
Luca Longinotti <l@longi.li>
2017-05-27 18:53:50 +0200

    ImageGenerator: use new way to get size in Init().

--- modules/imagegenerator/imagegenerator.c
+++ modules/imagegenerator/imagegenerator.c
@@ -13,10 +13,11 @@
 struct imagegenerator_state {
 	bool rectifyPolarities;
 	uint8_t colorScale;
-	// Image matrix.
-	simple2DBufferLong outputFrame;
+	simple2DBufferLong outputFrame; // Image matrix.
 	int32_t numSpikes; // After how many spikes will we generate an image.
 	int32_t spikeCounter; // Actual number of spikes seen so far, in range [0, numSpikes].
+	int16_t polaritySizeX;
+	int16_t polaritySizeY;
 };
 
 typedef struct imagegenerator_state *imagegeneratorState;
@@ -43,7 +44,15 @@ caerModuleInfo caerModuleGetInfo(void) {
 }
 
 static bool caerImageGeneratorInit(caerModuleData moduleData) {
-	imagegeneratorState state = moduleData->moduleState;
+	// Wait for input to be ready. All inputs, once they are up and running, will
+	// have a valid sourceInfo node to query, especially if dealing with data.
+	int16_t *inputs = caerMainloopGetModuleInputIDs(moduleData->moduleID, NULL);
+	if (inputs == NULL) {
+		return (false);
+	}
+
+	int16_t sourceID = inputs[0];
+	free(inputs);
 
 	sshsNodeCreateInt(moduleData->moduleNode, "numSpikes", 2000, 0, 200000, SSHS_FLAGS_NORMAL,
 		"Number of spikes to accumulate.");
@@ -55,9 +64,19 @@ static bool caerImageGeneratorInit(caerModuleData moduleData) {
 	sshsNodeCreateShort(moduleData->moduleNode, "outputFrameSizeY", 32, 1, 1024, SSHS_FLAGS_NORMAL,
 		"Output frame height.");
 
-	state->numSpikes = sshsNodeGetInt(moduleData->moduleNode, "numSpikes");
-	state->rectifyPolarities = sshsNodeGetBool(moduleData->moduleNode, "rectifyPolarities");
-	state->colorScale = U8T(sshsNodeGetShort(moduleData->moduleNode, "colorScale"));
+	imagegeneratorState state = moduleData->moduleState;
+
+	// Wait for source size information to be available.
+	// Allocate map using info from sourceInfo.
+	sshsNode sourceInfo = caerMainloopGetSourceInfo(sourceID);
+	if (sourceInfo == NULL) {
+		return (false);
+	}
+
+	state->polaritySizeX = sshsNodeGetShort(sourceInfo, "polaritySizeX");
+	state->polaritySizeY = sshsNodeGetShort(sourceInfo, "polaritySizeY");
+
+	caerImageGeneratorConfig(moduleData);
 
 	int16_t outputFrameSizeX = sshsNodeGetShort(moduleData->moduleNode, "outputFrameSizeX");
 	int16_t outputFrameSizeY = sshsNodeGetShort(moduleData->moduleNode, "outputFrameSizeY");
@@ -69,10 +88,10 @@ static bool caerImageGeneratorInit(caerModuleData moduleData) {
 	}
 
 	sshsNode sourceInfoNode = sshsGetRelativeNode(moduleData->moduleNode, "sourceInfo/");
-	sshsNodeCreateShort(sourceInfoNode, "dataSizeX", outputFrameSizeX, 1, 1024,
-		SSHS_FLAGS_READ_ONLY_FORCE_DEFAULT_VALUE, "Data output width.");
-	sshsNodeCreateShort(sourceInfoNode, "dataSizeY", outputFrameSizeY, 1, 1024,
-		SSHS_FLAGS_READ_ONLY_FORCE_DEFAULT_VALUE, "Data output height.");
+	sshsNodeCreateShort(sourceInfoNode, "frameSizeX", outputFrameSizeX, 1, 1024,
+		SSHS_FLAGS_READ_ONLY_FORCE_DEFAULT_VALUE, "Output frame width.");
+	sshsNodeCreateShort(sourceInfoNode, "frameSizeY", outputFrameSizeY, 1, 1024,
+		SSHS_FLAGS_READ_ONLY_FORCE_DEFAULT_VALUE, "Output frame height.");
 
 	// Add config listeners last, to avoid having them dangling if Init doesn't succeed.
 	sshsNodeAddAttributeListener(moduleData->moduleNode, moduleData, &caerModuleConfigDefaultListener);
@@ -113,19 +132,10 @@ static void caerImageGeneratorRun(caerModuleData moduleData, caerEventPacketCont
 		return;
 	}
 
-	// Wait for source size information to be available.
-	sshsNode sourceInfo = caerMainloopGetSourceInfo(caerEventPacketHeaderGetEventSource(&polarity->packetHeader));
-	if (sourceInfo == NULL) {
-		return;
-	}
-
-	float cam_sizeX = sshsNodeGetShort(sourceInfo, "polaritySizeX");
-	float cam_sizeY = sshsNodeGetShort(sourceInfo, "polaritySizeY");
-
 	imagegeneratorState state = moduleData->moduleState;
 
-	float res_x = (float) state->outputFrame->sizeX / cam_sizeX;
-	float res_y = (float) state->outputFrame->sizeY / cam_sizeY;
+	float res_x = (float) state->outputFrame->sizeX / state->polaritySizeX;
+	float res_y = (float) state->outputFrame->sizeY / state->polaritySizeY;
 
 	int32_t numtotevs = caerEventPacketHeaderGetEventValid(&polarity->packetHeader);
 
