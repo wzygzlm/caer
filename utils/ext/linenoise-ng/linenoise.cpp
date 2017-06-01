96f0ccb866e36a81228793d23fd93940e9af1281
Luca Longinotti <l@longi.li>
2017-06-01 17:28:45 +0200

    Linenoise-NG: fix new-lines.

--- utils/ext/linenoise-ng/linenoise.cpp
+++ utils/ext/linenoise-ng/linenoise.cpp
@@ -430,9 +430,9 @@ static int write32(int fd, char32_t* text32, int len32) {
 
 class Utf32String {
  public:
-  Utf32String() : _length(0), _data(nullptr) { 
+  Utf32String() : _length(0), _data(nullptr) {
     // note: parens intentional, _data must be properly initialized
-    _data = new char32_t[1](); 
+    _data = new char32_t[1]();
   }
 
   explicit Utf32String(const char* src) : _length(0), _data(nullptr) {
@@ -464,9 +464,9 @@ class Utf32String {
     memcpy(_data, src, len * sizeof(char32_t));
   }
 
-  explicit Utf32String(int len) : _length(0), _data(nullptr) { 
+  explicit Utf32String(int len) : _length(0), _data(nullptr) {
     // note: parens intentional, _data must be properly initialized
-    _data = new char32_t[len](); 
+    _data = new char32_t[len]();
   }
 
   explicit Utf32String(const Utf32String& that) : _length(that._length), _data(nullptr) {
@@ -2535,7 +2535,7 @@ int InputBuffer::getInputLine(PromptBase& pi) {
     if (terminatingKeystroke == -1) {
       c = linenoiseReadChar();  // get a new keystroke
 
-      keyType = 0; 
+      keyType = 0;
       if (c != 0) {
         // set flag that we got some input
         if (c == ctrlChar('C')) {
