cadc3c1d872f1605317c777f32a87cb42df5bcc5
Luca Longinotti <l@longi.li>
2017-08-23 12:24:39 +0200

    Visualizer: add useful helpers to work with SFML.

---
+++ modules/visualizer/ext/sfml/helpers.hpp
@@ -0,0 +1,32 @@
+#ifndef EXT_SFML_HELPERS_HPP_
+#define EXT_SFML_HELPERS_HPP_
+
+#include <SFML/Graphics.hpp>
+
+namespace sfml {
+class Helpers {
+public:
+	static void setOriginToCenter(sf::Shape &shape) {
+		shape.setOrigin(sf::Vector2f(shape.getLocalBounds().width / 2.0f, shape.getLocalBounds().height / 2.0f));
+	}
+
+	static void addPixelVertices(std::vector<sf::Vertex> &vec, const sf::Vector2f &position, const sf::Color &color =
+		sf::Color::White) {
+		sf::Vertex vtx(position);
+
+		vtx.color = color;
+
+		// Quads need four vertices. Color stays the same.
+		// Position changes by one in the clockwise sense.
+		vec.push_back(vtx);
+		vtx.position.x++;
+		vec.push_back(vtx);
+		vtx.position.y++;
+		vec.push_back(vtx);
+		vtx.position.x--;
+		vec.push_back(vtx);
+	}
+};
+}
+
+#endif /* EXT_SFML_HELPERS_HPP_ */
