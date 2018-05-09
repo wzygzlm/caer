48e6a45b4102e269c96e7feca4bd1d513025e975
Luca Longinotti <l@longi.li>
2018-05-09 23:23:07 +0200

    Mainloop: implement caerMainloopModuleGetInputDeps().

--- src/mainloop_sdk.cpp
+++ src/mainloop_sdk.cpp
@@ -38,15 +38,61 @@ sshsNode caerMainloopModuleGetConfigNode(int16_t id) {
 }
 
 size_t caerMainloopModuleGetInputDeps(int16_t id, int16_t **inputDepIds) {
+	// Ensure is set to NULL for error return.
+	// Support passing in NULL directly if not interested in result.
+	if (inputDepIds != nullptr) {
+		*inputDepIds = nullptr;
+	}
 
+	// Only makes sense to be called from PROCESSORs or OUTPUTs, as INPUTs
+	// do not have inputs themselves.
+	if (caerMainloopModuleGetType(id) == CAER_MODULE_INPUT) {
+		return (0);
+	}
+
+	std::vector<int16_t> inputModuleIds(glMainloopDataPtr->modules.at(id).inputDefinition.size());
+
+	// Get all module IDs of inputs to this module (each present only once in
+	// 'inputDefinition' of module), then sort them and return if so requested.
+	for (auto &in : glMainloopDataPtr->modules.at(id).inputDefinition) {
+		inputModuleIds.push_back(in.first);
+	}
+
+	std::sort(inputModuleIds.begin(), inputModuleIds.end());
+
+	if (inputDepIds != nullptr && !inputModuleIds.empty()) {
+		*inputDepIds = (int16_t *) malloc(inputModuleIds.size() * sizeof(int16_t));
+		if (*inputDepIds == nullptr) {
+			// Memory allocation failure, report as if nothing found.
+			return (0);
+		}
+
+		memcpy(*inputDepIds, inputModuleIds.data(), inputModuleIds.size() * sizeof(int16_t));
+	}
+
+	return (inputModuleIds.size());
 }
 
 size_t caerMainloopModuleGetOutputRevDeps(int16_t id, int16_t **outputRevDepIds) {
+	// Ensure is set to NULL for error return.
+	// Support passing in NULL directly if not interested in result.
+	if (outputRevDepIds != nullptr) {
+		*outputRevDepIds = nullptr;
+	}
 
+	// Only makes sense to be called from INPUTs or PROCESSORs, as OUTPUTs
+	// do not have outputs themselves.
+	if (caerMainloopModuleGetType(id) == CAER_MODULE_OUTPUT) {
+		return (0);
+	}
+
+	// TODO: *.
+
+	return (0);
 }
 
 void caerMainloopModuleResetOutputRevDeps(int16_t sourceID) {
-
+	// TODO: *.
 }
 
 static inline caerModuleData caerMainloopGetSourceData(int16_t sourceID) {
